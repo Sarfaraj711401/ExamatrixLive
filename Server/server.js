1c1a3b93d63bc027037790d80a4e16e6cd389607
Sarfaraj711401 <yourgithubemail@gmail.com>
2026-07-10 02:10:28 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://examatrix-live.vercel.app"
+  ]
+}));
 // 👉 JSON middleware
 app.use(express.json());
 
